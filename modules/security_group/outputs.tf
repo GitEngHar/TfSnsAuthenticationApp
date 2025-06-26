3c674fda34dfa4e30ada7de02eeb4c36b5cf34f2
GitEngHar <harukii1999@gmail.com>
2025-06-26 18:41:08 +0900

    add: lambda sg

--- modules/security_group/outputs.tf
+++ modules/security_group/outputs.tf
@@ -1,9 +1,9 @@
 output "ecs_service_access_security_group_id" {
-  value = aws_security_group.app-ecs.id
+  value = aws_security_group.app_ecs_sg.id
 }
 
 output "alb_access_security_group_id" {
-  value = aws_security_group.app-alb.id
+  value = aws_security_group.app_alb_sg.id
 }
 
 output "mysql_access_security_group_id" {
